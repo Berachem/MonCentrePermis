0385766cab1bc827e511cf1cc292c1974ca18b63
Berachem Markria <berachem.markria@gmail.com>
2024-10-30 22:43:47 +0100

    Update vite.config.ts

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: '0.0.0.0', // Permet d'accepter les connexions externes
+    port: 3000,       // Choisissez le port que vous souhaitez, ici 3000
+  },
 })
