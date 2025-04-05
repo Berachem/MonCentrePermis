75e88ee7c22104084cfba561ade2c81a756f94fe
Kilouhardi <Joshual.courriel@gmail.com>
2025-04-05 16:34:18 +0200

    Profil Moniteur OK

--- frontend/src/pages/Profile.tsx
+++ frontend/src/pages/Profile.tsx
@@ -3,14 +3,18 @@ import useAuth from "../hooks/useAuth";
 import { useNavigate } from "react-router-dom";
 import { Avatar } from 'primereact/avatar';
 import { Card } from 'primereact/card';
-import { Button } from 'primereact/button'; // Importation de PrimeReact Button
+import { Button } from 'primereact/button';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faEdit } from '@fortawesome/free-solid-svg-icons';
-import { Fieldset } from 'primereact/fieldset';
+import { faEdit,faChartBar,faQuestionCircle } from '@fortawesome/free-solid-svg-icons';
+import { Tooltip } from 'primereact/tooltip';
+
 
 const Profile: React.FC = () => {
   const { isAuthenticated, prenom, nom, logout } = useAuth();
   const navigate = useNavigate()
+  const [studentCount, setStudentCount] = useState<number | null>(null);
+  const [viewCount, setViewCount] = useState<number | null>(null);
+  const [rating, setRating] = useState<number | null>(null);
   const [courseCount, setCourseCount] = useState<number | null>(null)
   const [description, setDescription] = useState('');
   const [newDescription, setNewDescription] = useState(''); //si l'utilisateur modifie sa desc
@@ -29,7 +33,22 @@ const Profile: React.FC = () => {
 
   const fetchCourseCount = async () => {
     // TODO : connecter le back
-    return 99
+    return 145
+  };
+
+  const fetchStudentCount = async (): Promise<number> => {
+    // TODO: connecter au backend
+    return 10;
+  };
+
+  const fetchViewCount = async (): Promise<number> => {
+    // TODO: connecter au backend
+    return 20;
+  };
+
+  const fetchRating = async (): Promise<number> => {
+    // TODO: connecter au backend
+    return 4;
   };
 
   useEffect(() => {
@@ -38,20 +57,25 @@ const Profile: React.FC = () => {
       navigate("/")
     }
 
-    // récupération de la description
-    const loadDescription = async () => {
-      const desc = await fetchUserDescription()
-      setDescription(desc)
-      setNewDescription(desc) //pour la modification de desc
-    };
-
-    const loadCourseCount = async () => {
-      const count = await fetchCourseCount()
-      setCourseCount(count)
+    const loadUserData = async () => {
+      const [desc, course, students, views, rate] = await Promise.all([
+        fetchUserDescription(),
+        fetchCourseCount(),
+        fetchStudentCount(),
+        fetchViewCount(),
+        fetchRating(),
+      ]);
+  
+      setDescription(desc);
+      setNewDescription(desc);
+      setCourseCount(course);
+      setStudentCount(students);
+      setViewCount(views);
+      setRating(rate);
     };
   
-    loadDescription()
-    loadCourseCount()
+  
+    loadUserData()
   }, [isAuthenticated, navigate])
 
   const handleEditClick = () => {
@@ -107,36 +131,83 @@ const Profile: React.FC = () => {
             </div>
           )}
         </div>
+      </Card>
 
-        <div className="flex flex-column whitespace-nowrap md:whitespace-normal md:flex-row gap-3 w-full mt-4">
-          <Fieldset className="w-full md:w-4">
-            <div className="flex flex-column ">
-              <span className="mx-auto w-full">
-                <span className="text-indigo-600  text-6xl my-2">
-                  {(courseCount !== null ? courseCount : '0')}
-                </span>
-                <span className="text-xl ml-2">
-                  cours créés
-                </span>
+      <h2 className="text-2xl font-semibold mt-4 mb-2 md:mx-8 flex items-center">
+        <FontAwesomeIcon icon={faChartBar} className="mr-2 text-indigo-600" />
+        Mes statistiques
+      </h2>
+      <div className="flex flex-column md:flex-row gap-3 mt-4 md:mx-8">
+        <div className="relative w-full md:w-6">
+          <Card className="w-full relative overflow-hidden">
+            <i
+              className={`pi pi-book text-indigo-200 text-8xl absolute top-0 right-0 m-3 pointer-events-none`}
+            ></i>
+
+            <div className="flex flex-column">
+              <span className="text-indigo-600 text-6xl my-2">
+                {courseCount !== null ? courseCount : '0'}
               </span>
+              <span className="text-xl ml-2">cours créés</span>
               <Button
                 label="Voir mes cours"
                 className="button-text text-sm mr-auto mt-3 md:mt-4"
-                onClick={() => console.log("Voir les cours")}
+                onClick={() => console.log('Voir les cours')}
               />
             </div>
-          </Fieldset>
-
-          <Fieldset legend="Statistiques" className="w-full md:w-8">
-            {/* Contenu du deuxième fieldset */}
-          </Fieldset>
+          </Card>
         </div>
 
-      </Card>
+        <div className="relative w-full md:w-6"> 
+          <div className="relative">
+            <Card>
+              <Tooltip target=".eleve-tooltip" />
+                  <FontAwesomeIcon
+                        icon={faQuestionCircle}
+                        className="eleve-tooltip text-right text-sm cursor-pointer absolute top-0 right-0 m-2"
+                        data-pr-tooltip="Nombre d'élèves inscrits a vos cours"
+                  />
+                <div className="flex flex-column align-items-start relative">
+                  <i className="pi pi-users text-indigo-200 text-7xl absolute top-0 right-0 m-2 pointer-events-none"></i>
+                  <span className="text-indigo-600 text-4xl my-2">{studentCount ?? '...'}</span>
+                  <span className="text-xl text-gray-500">Élèves</span>
+                </div>
+            </Card>
+          </div>
 
-      
-        
-    
+          <div className="relative">
+            <Card className="mt-2">
+                <Tooltip target=".vues-tooltip" />
+                <FontAwesomeIcon
+                      icon={faQuestionCircle}
+                      className="vues-tooltip text-right text-sm cursor-pointer absolute top-0 right-0 m-2"
+                      data-pr-tooltip="Nombre de fois que vos cours on été consultés"
+                />
+                <div className="flex flex-column align-items-start relative">
+                  <i className="pi pi-eye text-green-200 text-7xl absolute top-0 right-0 m-2 pointer-events-none"></i>
+                  <span className="text-green-600 text-4xl my-2">{viewCount ?? '...'}</span>
+                  <span className="text-xl text-gray-500">Vues</span>
+                </div>
+            </Card>
+          </div>
+
+          <div className="relative">
+            <Card className="mt-2">
+                <Tooltip target=".note-tooltip" />
+                <FontAwesomeIcon
+                      icon={faQuestionCircle}
+                      className="note-tooltip text-right text-sm cursor-pointer absolute top-0 right-0 m-2"
+                      data-pr-tooltip="Note moyenne par rapport a toutes les évaluations laissées par vos élèves"
+                />
+                <div className="flex flex-column align-items-start relative">
+                  <i className="pi pi-star text-yellow-200 text-7xl absolute top-0 right-0 m-2 pointer-events-none"></i>
+                  <span className="text-yellow-500 text-4xl my-2">4 / 5</span>
+                  <span className="text-xl text-gray-500">Note</span>
+                </div>
+            </Card>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
